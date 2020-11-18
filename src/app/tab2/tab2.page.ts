96a7c52e770d23f4bace1f6573e6a5d5eee4f067
Nilson Arana <eedeenilson@gmail.com>
2020-11-18 13:56:53 -0600

    Agrega get method para recurso humano y tambien agrega funcionalidad en la vista recurso humano

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoadingController, NavController } from '@ionic/angular';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  recursoHumano;
+  constructor(
+    private http: HttpClient,
+    public loadingController: LoadingController,
+    public navController: NavController
+  ) {
+    this.http.get(`${environment.urlApi}/recursohumano`, {
+    }).subscribe(
+      data => {
+        this.recursoHumano = data;
+        console.log(this.recursoHumano);
 
-  constructor() {}
+      },
+      error => {
+        console.log("Error al ejecutar http get");
+      }
+    );
+  }
+
+  nuevoRecurso() {
+    this.navController.navigateForward(['/insert-rh']);
+  }
 
 }
