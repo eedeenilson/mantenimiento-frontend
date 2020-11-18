fd8522547d25bd64e41582076a3374da27f8556d
Nilson Arana <eedeenilson@gmail.com>
2020-11-18 13:48:07 -0600

    Agrega nuevas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,26 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'insert-proveedor',
+    loadChildren: () => import('./insert-proveedor/insert-proveedor.module').then( m => m.InsertProveedorPageModule)
+  },
+  {
+    path: 'insert-rh',
+    loadChildren: () => import('./insert-rh/insert-rh.module').then( m => m.InsertRHPageModule)
+  },
+  {
+    path: 'insert-equipo',
+    loadChildren: () => import('./insert-equipo/insert-equipo.module').then( m => m.InsertEquipoPageModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+  },
+  {
+    path: 'insert-orden',
+    loadChildren: () => import('./insert-orden/insert-orden.module').then( m => m.InsertOrdenPageModule)
   }
 ];
 @NgModule({
