a51acc7ad5bf9609fb899604213f0d33a0a35358
Nilson Arana <eedeenilson@gmail.com>
2020-12-01 09:32:05 -0600

    Funcionalidad para editar orden

--- src/app/edit-orden/edit-orden.module.ts
+++ src/app/edit-orden/edit-orden.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +13,9 @@ import { EditOrdenPage } from './edit-orden.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    EditOrdenPageRoutingModule
+    EditOrdenPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [EditOrdenPage]
 })
-export class EditOrdenPageModule {}
+export class EditOrdenPageModule { }
--- src/app/edit-orden/edit-orden.page.ts
+++ src/app/edit-orden/edit-orden.page.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController, ToastController } from '@ionic/angular';
+import { environment } from '../../environments/environment';
+import * as moment from 'moment'
 
 @Component({
   selector: 'app-edit-orden',
@@ -6,10 +11,131 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-orden.page.scss'],
 })
 export class EditOrdenPage implements OnInit {
+  queryData;
+  orden;
 
-  constructor() { }
+  mantenimiento;
+  equipo;
+  falla;
+  recursoHumano;
+  fechaG;
+  fechaAct;
+  observaciones = "";
+  proceso = 1;
+  procedimientos;
+  constructor(
+    public activatedRoute: ActivatedRoute,
+    public http: HttpClient,
+    public toastController: ToastController,
+    public navController: NavController
+  ) {
+
+  }
 
   ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((data) => {
+      this.queryData = data;
+      console.log(this.queryData);
+      this.getOrdenById(this.queryData.id);
+      this.getProcedimintos();
+    });
+  }
+
+  getOrdenById(id) {
+    this.http.get(`${environment.urlApi}/ordentrabajo/ordenes/${id}`, {
+    }).subscribe(
+      data => {
+        this.orden = data;
+        console.log(this.orden);
+        this.asignarOrden(this.orden);
+      },
+      error => {
+        console.log("Error al ejecutar http get");
+      }
+    );
+  }
+
+  asignarOrden(orden) {
+    this.mantenimiento = orden.idTipoMantenimiento.mantenimiento;
+    this.equipo = orden.idEquipo.equipo;
+    this.falla = orden.observaciones;
+    this.recursoHumano = `${orden.idRecursoHumano.apellido} ${orden.idRecursoHumano.nombre}`
+    this.fechaG = this.orden.fechaGeneracion.slice(0, -5);
+    this.fechaAct = moment().format();
+  }
+
+  getProcedimintos() {
+    this.http.get(`${environment.urlApi}/procedimiento`, {
+    }).subscribe(
+      data => {
+        this.procedimientos = data;
+        console.log(this.procedimientos);
+      },
+      error => {
+        console.log("Error al ejecutar http get");
+      }
+    );
+  }
+
+  limpiar() {
+    this.navController.navigateRoot(['/ordentrabajo'])
+  }
+
+  editar() {
+    let newOrden = {
+      fechaActualizacion: this.fechaAct,
+      fechaGeneracion: this.fechaG,
+      idEquipo: {
+        idEquipo: this.orden.idEquipo.idEquipo
+      },
+      idEstado: {
+        idEstado: 2
+      },
+      idOrdenTrabajo: this.orden.idOrdenTrabajo,
+      idRecursoHumano: {
+        idRecursoHumano: this.orden.idRecursoHumano.idRecursoHumano
+      },
+      idTipoMantenimiento: {
+        idTipoMantenimiento: this.orden.idTipoMantenimiento.idTipoMantenimiento
+      },
+      idProcedimiento: {
+        idProcedimiento: this.proceso
+      },
+      observaciones: this.falla + " " + this.observaciones
+    }
+    this.http.put(`${environment.urlApi}/ordentrabajo/${this.orden.idOrdenTrabajo}`, newOrden).subscribe(
+      data => {
+        console.log("Se ha insertado");
+        this.showToast();
+        this.limpiar();
+      },
+      error => {
+        console.log("Error al ejecutar http post");
+      }
+    );
+  }
+
+  showToast() {
+    this.toastController.create({
+      color: 'success',
+      duration: 2000,
+      header: 'Agregado',
+      message: 'Orden de trabajo modificada',
+      position: 'middle',
+      cssClass: 'my-custom-class',
+      buttons: [
+        {
+          side: 'end',
+          text: 'Close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Alerta cerrada');
+          }
+        }
+      ]
+    }).then((obj) => {
+      obj.present();
+    });
   }
 
 }
