e28daa814b19428ae77b5bd832000ebf72e9ba2d
Nilson Arana <eedeenilson@gmail.com>
2020-12-01 09:36:01 -0600

    Agrega funcionalidad para orden de trabajo inactivas

--- src/app/ordentrabajo-inactivas/ordentrabajo-inactivas.page.ts
+++ src/app/ordentrabajo-inactivas/ordentrabajo-inactivas.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-ordentrabajo-inactivas',
@@ -6,8 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ordentrabajo-inactivas.page.scss'],
 })
 export class OrdentrabajoInactivasPage implements OnInit {
+  ordenes;
+  ordenTrabajo = [];
+  constructor(
+    private http: HttpClient
+  ) {
+    this.http.get(`${environment.urlApi}/ordentrabajo/ordenes`, {
+    }).subscribe(
+      data => {
+        this.ordenes = data;
+        this.ordenes.reverse()
+        for (const iterator of this.ordenes) {
+          iterator.fechaGeneracion = iterator.fechaGeneracion.slice(0, -5);
+          iterator.fechaActualizacion ? iterator.fechaActualizacion = iterator.fechaActualizacion.slice(0, -5) : "1970-01-01 00:00:00"
+          this.ordenTrabajo.push(iterator)
+        }
 
-  constructor() { }
+        // console.log(this.ordenes);
+
+      },
+      error => {
+        console.log("Error al ejecutar http get");
+      }
+    );
+  }
 
   ngOnInit() {
   }
