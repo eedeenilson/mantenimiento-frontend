1f7b60a4002bad8c7597f8b9a2620ff4e2a755c8
Nilson Arana <eedeenilson@gmail.com>
2020-12-01 09:35:30 -0600

    Agrega funcionalidad para orden de trabajo activas

--- src/app/ordentrabajo/ordentrabajo.page.ts
+++ src/app/ordentrabajo/ordentrabajo.page.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-ordentrabajo',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdentrabajoPage implements OnInit {
 
-  constructor() { }
+  ordenes;
+  ordenTrabajo = [];
+  constructor(
+    private http: HttpClient,
+    public navController: NavController
+  ) {
+
+    this.http.get(`${environment.urlApi}/ordentrabajo/ordenes`, {
+    }).subscribe(
+      data => {
+        this.ordenes = data;
+        this.ordenes.reverse()
+        for (const iterator of this.ordenes) {
+          iterator.fechaGeneracion = iterator.fechaGeneracion.slice(0, -5);
+          this.ordenTrabajo.push(iterator)
+        }
+
+        // console.log(this.ordenes);
+
+      },
+      error => {
+        console.log("Error al ejecutar http get");
+      }
+    );
+
+  }
 
   ngOnInit() {
+
   }
+
+  editOrden(id) {
+    this.navController.navigateRoot(['/edit-orden/'], { queryParams: { id: id } });
+  }
+
 
 }
