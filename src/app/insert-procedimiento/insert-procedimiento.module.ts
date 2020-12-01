ac95bc4118c22d75f245904dc6e55cee7cba7fd4
Nilson Arana <eedeenilson@gmail.com>
2020-12-01 09:34:39 -0600

    Agrega funcionalidad para insertar procedimiento

--- src/app/insert-procedimiento/insert-procedimiento.module.ts
+++ src/app/insert-procedimiento/insert-procedimiento.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +13,9 @@ import { InsertProcedimientoPage } from './insert-procedimiento.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    InsertProcedimientoPageRoutingModule
+    InsertProcedimientoPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [InsertProcedimientoPage]
 })
-export class InsertProcedimientoPageModule {}
+export class InsertProcedimientoPageModule { }
--- src/app/insert-procedimiento/insert-procedimiento.page.ts
+++ src/app/insert-procedimiento/insert-procedimiento.page.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController, ToastController } from '@ionic/angular';
+import { environment } from '../../environments/environment'
 
 @Component({
   selector: 'app-insert-procedimiento',
@@ -6,10 +10,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./insert-procedimiento.page.scss'],
 })
 export class InsertProcedimientoPage implements OnInit {
-
-  constructor() { }
+  procedimientoForm: FormGroup
+  constructor(
+    public toastController: ToastController,
+    public navController: NavController,
+    public fb: FormBuilder,
+    public http: HttpClient
+  ) {
+    this.procedimientoForm = fb.group({
+      'txtProcedimiento': [null, Validators.required],
+      'txtDescripcion': [null, Validators.required],
+      'txtObservaciones': [null, Validators.required]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  registrar() {
+    let value = this.procedimientoForm.value;
+    for (let c in this.procedimientoForm.controls) {
+      this.procedimientoForm.controls[c].markAsTouched();
+    }
+
+    if (this.procedimientoForm.valid) {
+      this.http.post(`${environment.urlApi}/procedimiento`, {
+
+        "procedimiento": value.txtProcedimiento,
+        "observaciones": value.txtObservaciones,
+        "descripcion": value.txtDescripcion
+
+      }).subscribe(
+        data => {
+          console.log("Se ha insertado");
+          this.showToast()
+          this.procedimientoForm.reset();
+          this.navController.navigateRoot(['/tabs/procedimiento'])
+        },
+        error => {
+          console.log("Error al ejecutar http get");
+        }
+      );
+    }
+  }
+
+  limpiar() {
+    this.navController.navigateRoot(['/tabs/tab1'])
+  }
+
+  showToast() {
+    this.toastController.create({
+      color: 'success',
+      duration: 2000,
+      header: 'Agregado',
+      message: 'Nuevo equipo agregado',
+      position: 'middle',
+      cssClass: 'my-custom-class',
+      buttons: [
+        {
+          side: 'end',
+          text: 'Close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Alerta cerrada');
+          }
+        }
+      ]
+    }).then((obj) => {
+      obj.present();
+    });
   }
 
 }
