c6883c631b259d7302a37789341d3c8e3925f435
Nilson Arana <eedeenilson@gmail.com>
2020-12-01 09:37:19 -0600

    Agrega ruta y tab procedimiento

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'procedimiento',
+        loadChildren: () => import('../procedimiento/procedimiento.module').then(m => m.ProcedimientoPageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
@@ -37,4 +41,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class TabsPageRoutingModule {}
+export class TabsPageRoutingModule { }
